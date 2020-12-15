ad1d2f7da29a98f19e3bbd824fb948efea40564f
Arenbo <34847737+Arenbo@users.noreply.github.com>
2020-12-15 17:26:15 +0500

    examples for tails

--- src/generator/index.js
+++ src/generator/index.js
@@ -17,8 +17,8 @@ module.exports = {
 // 8
 // 9
 // 10
-// 11
-// 12 color
+// 11 color
+// 12
 // 13
 // 14
 // 15 scale color
@@ -32,8 +32,26 @@ module.exports = {
 // 23 claws color
 // 24 eyes
 // 25 eyes color
-// let gens = '0-12-4-4-2-33-3-3-0-1-321-213-6-100-14-45-5-22-1-55-204-67-5-9-3-76-55-13-1-2-6-3';
-// let genDragon = new GenDragon(gens, 104);
 
-// genDragon.onGenerateFragments();
+// example - 7 types of tails
+
+// let gens = '0-12-4-4-2-33-3-0-0-1-321-213-6-100-0-0-1-22-1-55-204-0-5-9-3-76-55-13-1-2-6-3';
+// let genDragon = new GenDragon(gens, 400);
+// let gens = '0-12-4-4-2-33-3-1-0-1-321-213-6-100-0-0-1-22-1-55-204-0-5-9-3-76-55-13-1-2-6-3';
+// let genDragon = new GenDragon(gens, 401);
+let gens = '0-12-4-4-2-33-3-2-0-1-321-213-6-100-0-0-1-22-1-55-204-0-5-9-3-76-55-13-1-2-6-3';
+let genDragon = new GenDragon(gens, 402);
+// let gens = '0-12-4-4-2-33-3-3-0-1-321-213-6-100-0-0-1-22-1-55-204-0-5-9-3-76-55-13-1-2-6-3';
+// let genDragon = new GenDragon(gens, 403);
+// let gens = '0-12-4-4-2-33-3-4-0-1-321-213-6-100-0-0-1-22-1-55-204-0-5-9-3-76-55-13-1-2-6-3';
+// let genDragon = new GenDragon(gens, 404);
+// let gens = '0-12-4-4-2-33-3-5-0-1-321-213-6-100-0-0-1-22-1-55-204-0-5-9-3-76-55-13-1-2-6-3';
+// let genDragon = new GenDragon(gens, 405);
+// let gens = '0-12-4-4-2-33-3-6-0-1-321-213-6-100-0-0-1-22-1-55-204-0-5-9-3-76-55-13-1-2-6-3';
+// let genDragon = new GenDragon(gens, 406);
+// let gens = '0-12-4-4-2-33-3-7-0-1-321-213-6-100-0-0-1-22-1-55-204-0-5-9-3-76-55-13-1-2-6-3';
+// let genDragon = new GenDragon(gens, 407);
+
+
+genDragon.onGenerateFragments();
 // need fixed tails no working details.
